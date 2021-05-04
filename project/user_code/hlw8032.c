8a15e5110a1a145693f51e58a74dc7442a5fbad5
bxgj <bwysxl@126.com>
2021-05-05 01:40:36 +0800

    更新hlw8032驱动程序

--- project/user_code/hlw8032.c
+++ project/user_code/hlw8032.c
@@ -1,12 +1,13 @@
 #include "hlw8032.h"
 
 #include "stm32g0xx_ll_gpio.h"
-#include "user_hw_init.h"
+#include "user_utils.h"
 
 #include <stdio.h>
+#include <string.h>
 
 /*
-示例数据
+测试数据
 状态寄存器
 检测寄存器
 电压参数寄存器
@@ -20,42 +21,6 @@ PF寄存器
 校验和寄存器
 
 0xF2
-
-0x5A
-
-0x02
-0xDA
-0x78
-
-0x07
-0x2E
-0x2C
-
-0x00
-0x3D
-0xD6
-
-0x07
-0x2D
-0x94
-
-0x4E
-0x0C
-0x78
-
-0x55
-0x7B
-0xEE
-
-0x61
-
-0x00
-0x03
-
-0x84
-
-
-0xF2
 0x5A
 0x02DA78
 0x072E2C
@@ -66,9 +31,6 @@ PF寄存器
 0x61
 0x0003
 0x84
-
-0x02DA78+0x072E2C+0x003DD6+0x072D94+0x4E0C78+0x557BEE
-
 */
 
 // 电压、电流、功率系数，如果更换采样电阻、互感器等采样相关电路需要重新计算并校准，详见官方手册
@@ -94,16 +56,6 @@ typedef struct {
 	uint16_t pf;
 }hlw8032_reg;
 
-typedef struct {
-	// uint32_t state; // 0正常，1错误
-	float current;
-	float voltage;
-	float power_factor;
-	float active_power; // 有功功率
-	float apparent_power; // 视在功率
-	int32_t total_wh; // 总耗电量，注意这里的单位是wh
-}electric_info;
-
 static hlw8032_reg reg;
 static electric_info info;
 
@@ -115,48 +67,11 @@ static uint16_t last_pf;
 static int32_t kwh_pf_cnt = 0; // 每kwh的pf计数
 static int32_t wh_pf_cnt = 0; // 每wh的pf计数
 static int32_t pf_cnt = 0;
-// static float delta_wh = 0.0; // 两次测量之间的耗电量
 
-void hlw8032_init(void)
-{
-	// GPIO init
-	// 1 度电的脉冲数量 = (1 / 功率参数寄器) x (1 / (电压系数 x 电流系数)) x 10^9 x 3600
-	// skwh_pf_cnt = (10000000 / reg.power_param) * (100000 / voltage_coefficient / current_coefficient) * 36;
-	// kwh_pf_cnt = 123456; // 每1kwh的pf计数
-	// wh_pf_cnt = kwh_pf_cnt / 1000; // 每1kwh的pf计数
-
-	// info.total_wh = read_wh();
-}
-
-// 在串口接收中断函数中调用
-void hlw8032_recv(uint8_t dat)
-{
-	static uint32_t last_tick = 0;
-	static uint32_t pos = 0;
-	uint32_t new_tick = 0;
-
-	new_tick = HAL_GetTick();
-
-	// hlw8032 每50ms发送一次数据，每包数据24字节
-	if (new_tick - last_tick > 20)
-	{
-		pos = 0;
-	}
-	last_tick = new_tick;
-	recv_buf[pos] = dat;
-	pos += 1;
-	if (pos == 24)
-	{
-		hlw8032_analyse();
-		pos = 0;
-	}
-}
-
-void hlw8032_analyse(void)
+static void hlw8032_analyse(void)
 {
 	uint8_t check_sum = 0;
 	uint8_t i = 0;
-	extern uint8_t ch_g;
 
 	for (i = 2; i < 23; i++)
 	{
@@ -165,10 +80,8 @@ void hlw8032_analyse(void)
 
 	if (check_sum != recv_buf[23])
 	{
-		ch_g = 1;
 		return;
 	}
-	ch_g = 2;
 
 	reg.state         = recv_buf[0];
 	reg.check         = recv_buf[1];
@@ -259,55 +172,56 @@ void hlw8032_analyse(void)
 	}
 }
 
-void hlw8032_get_info(void)
+void hlw8032_init(uint32_t total_wh)
 {
-	printf("sta 0x%x, chk 0x%x, vp 0x%x, v 0x%x, cp 0x%x, c 0x%x, pp 0x%x, p 0x%x, du 0x%x, pf 0x%x \r\n",
-		reg.state, reg.check,
-		reg.voltage_param, reg.voltage,
-		reg.current_param, reg.current,
-		reg.power_param, reg.power,
-		reg.data_updata, reg.pf);
+	// GPIO init
+	// 1 度电的脉冲数量 = (1 / 功率参数寄器) x (1 / (电压系数 x 电流系数)) x 10^9 x 3600
+	// skwh_pf_cnt = (10000000 / reg.power_param) * (100000 / voltage_coefficient / current_coefficient) * 36;
+	// kwh_pf_cnt = 123456; // 每1kwh的pf计数
+	// wh_pf_cnt = kwh_pf_cnt / 1000; // 每1kwh的pf计数
 
-	printf("c %f, v %f, pf %f, p %f, ap %f, wh %d \r\n",
-	info.current, info.voltage, info.power_factor, info.active_power, info.apparent_power, info.total_wh);
-
-	printf("\r\n\r\n");
-
-	return;
+	info.total_wh = total_wh;
 }
 
-float hlw8032_get_current(void)
+// 在串口接收中断函数中调用
+void hlw8032_recv(uint8_t dat)
 {
-	return 0.0;
+	static uint32_t last_tick = 0;
+	static uint32_t pos = 0;
+	uint32_t new_tick = 0;
+
+	new_tick = HAL_GetTick();
+
+	// hlw8032 每50ms发送一次数据，每包数据24字节
+	if (new_tick - last_tick > 20)
+	{
+		pos = 0;
+	}
+	last_tick = new_tick;
+	recv_buf[pos] = dat;
+	pos += 1;
+	if (pos == 24)
+	{
+		hlw8032_analyse();
+		pos = 0;
+	}
 }
 
-float hlw8032_get_voltage(void)
+void hlw8032_get_info(electric_info *p_info)
 {
-	return 0.0;
-}
+	// printf("sta 0x%x, chk 0x%x, vp 0x%x, v 0x%x, cp 0x%x, c 0x%x, pp 0x%x, p 0x%x, du 0x%x, pf 0x%x \r\n",
+	// 	reg.state, reg.check,
+	// 	reg.voltage_param, reg.voltage,
+	// 	reg.current_param, reg.current,
+	// 	reg.power_param, reg.power,
+	// 	reg.data_updata, reg.pf);
 
-float hlw8032_get_power_factor(void)
-{
-	return 0.0;
-}
+	// printf("c %f, v %f, pf %f, p %f, ap %f, wh %d \r\n",
+	// info.current, info.voltage, info.power_factor, info.active_power, info.apparent_power, info.total_wh);
 
-float hlw8032_get_active_power(void)
-{
-	return 0.0;
-}
+	// printf("\r\n\r\n");
 
-float hlw8032_get_apparent_power(void)
-{
-	return 0.0;
-}
+	memcpy(p_info, &info, sizeof(electric_info));
 
-float hlw8032_get_total_wh(void)
-{
-	return 0.0;
-}
-
-void hlw8032_save_total_wh(void)
-{
-	// HAL_FLASH_Program(); // HAL 库每次写8字节，不适用
 	return;
 }
--- project/user_code/hlw8032.h
+++ project/user_code/hlw8032.h
@@ -3,17 +3,17 @@
 
 #include <stdint.h>
 
-// void hlw8032_init(void);
-void hlw8032_recv(uint8_t dat);
-void hlw8032_analyse(void);
-// float hlw8032_get_current(void);
-// float hlw8032_get_voltage(void);
-// float hlw8032_get_power_factor(void);
-// float hlw8032_get_active_power(void);
-// float hlw8032_get_apparent_power(void);
-void hlw8032_get_info(void);
+typedef struct {
+	float current;
+	float voltage;
+	float power_factor;
+	float active_power; // 有功功率
+	float apparent_power; // 视在功率
+	uint32_t total_wh; // 总耗电量，注意这里的单位是wh
+}electric_info;
 
-float hlw8032_get_total_kwh(void);
-void hlw8032_save_total_kwh(void);
+void hlw8032_init(uint32_t total_wh); // 初始化时传入累计用电量，之后会在这个基础上累加
+void hlw8032_recv(uint8_t dat);
+void hlw8032_get_info(electric_info *p_info);
 
 #endif
